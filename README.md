19258c01f194dbbac117895e605e150f982adc43
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2015-07-23 02:03:17 +0900

    Be able to run the demo app without sbt/scala installation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # javajo-spark-ml-demo
-A demo ML application using spark
+
+A demo ML application using spark.
+
+
+# How to use
+
+1. Run `./setup.sh` only once
+2. Do `./sbt run`
--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 rm smsspamcollection.zip SMSSpamCollection
+rm bin/sbt-launch.jar
 
 wget https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip
 unzip smsspamcollection.zip SMSSpamCollection
+
+mkdir bin
+wget -P bin http://dl.bintray.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.8/sbt-launch.jar
